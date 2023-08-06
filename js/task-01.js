effd84038d6296b2e31676cd1494a79156828bc1
Lifuss <arsen.hryh@gmail.com>
2023-08-06 15:37:03 +0300

    task1&2

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const quanityUl = document.querySelectorAll('.item')
+console.log('Number of categories: ', quanityUl.length);
+
+quanityUl.forEach((el) =>
+    console.log(`
+Category ${el.firstElementChild.textContent} 
+Elements: ${el.lastElementChild.children.length}`))
+
+// Done
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients')
+
+const ingredientListMarkup = ingredients.map(ingredient => {
+  const ingredientsItemEl = document.createElement("li")
+  ingredientsItemEl.textContent = ingredient
+  ingredientsItemEl.classList.add('item')
+  return ingredientsItemEl
+})
+ingredientsListEl.append(...ingredientListMarkup)
